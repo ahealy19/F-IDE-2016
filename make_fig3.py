117bdcdc18636237e89cfd8a7b477747b538ded0
Andrew Healy <andrew.healy.2014@mumail.ie>
2016-08-23 10:53:07 +0100

    fig3 stuff

---
+++ make_fig3.py
@@ -0,0 +1,81 @@
+import json, os, common
+from pandas import DataFrame, Series
+import matplotlib.pyplot as plt
+import numpy as np
+
+"""
+see how long it takes for each prover to return their useful results.
+Plot the cumulative number of these results as the timeout increases. 
+"""
+
+df = DataFrame.from_csv('whygoal_stats.csv')
+provers = common.PROVERS
+
+fig = plt.figure(figsize=(20,8))
+ax = fig.add_subplot(1,1,1)
+x = 0
+
+styles = {p : ['-k',':k','--k','-.k','-c','--c','-.c',':c'][i] for i, p in enumerate(provers)}
+
+here = os.getcwd()
+
+new_json = [ folder_name+'.json' for folder_name in 
+				os.listdir('data') if os.path.isdir(os.path.join('data', folder_name)) 
+				]
+
+df2 = {}
+# 5,10 covered by the whygoal_stats data
+for time in [15, 20, 25, 30, 60]:
+	df2[time] = {}
+	for p in provers:
+
+		df2[time][p] = 0
+
+		for jfile in sorted(new_json):
+			folder_name = jfile[:-5]
+			with open(os.path.join('data',folder_name, jfile)) as jf:
+				d = json.load(jf)
+			for _, fd in d.iteritems():
+				for _, td in fd.iteritems():
+					for _, gd in td.iteritems():
+						if gd[p][str(time)+'secs'] in ['Valid', 'Invalid', 'Unknown']:
+							df2[time][p] += 1
+
+		
+
+df2 = DataFrame(df2)
+
+total = {}
+fig = plt.figure()
+fig.set_size_inches(8, 4, forward=True)
+ax = fig.add_subplot(1,1,1)
+percentiles = {}
+
+for p in provers:
+	
+	# plotting up to 10secs
+	sorted_times = df[[p+' result', p+' time']].sort_values(by=[p+' time'])
+	good_times = sorted_times.apply(lambda ser: ser[p+' result'] in ['Valid', 'Invalid', 'Unknown'], axis=1)
+	# just use useful results
+	actual_times = sorted_times.loc[good_times]
+	# times are the index
+	times = Series([x+1 for x in range(actual_times.shape[0])], index=actual_times[p+' time'])
+	total[p] = times.append(df2.ix[p])
+	# 99th percentile of values
+	percentile99 = total[p].index[round(total[p].quantile(0.99))-1]
+	print p +' : '+ str(percentile99) + ' ('+str(total[p].values[-1])+')'
+	# uncomment this line to draw red lines for the 99 percentiles
+	#ax.plot((percentile99,percentile99),(0,total[p].quantile(0.99)),'r-')
+	ax.plot(total[p].index, total[p].values, styles[p],  label=p)
+	
+# the dotted vertical line
+ax.plot((10,10),(0,800),'k:')
+# to show more detail for very fast results
+ax.set_xscale('log',basex=2)
+ax.set_xlabel('Time (log 2 secs)')
+ax.set_ylabel('Number of Valid/Invalid/Unknown responses')
+ax.legend(loc='best', ncol=2)
+plt.savefig(os.path.join('paper','line_graph.pdf'), bbox_inches='tight')
+fig.show()
+
+#DataFrame(total).plot(0)
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 # Predicting SMT Performance for Software Verification
 ## Andrew Healy, Rosemary Monahan, James, F. Power
-### Dept. of Computer Science, Maynooth University, Ireland
+### [Principles of Programming](http://www.cs.nuim.ie/research/pop/) Research Group, Dept. of Computer Science, Maynooth University, Ireland
 
-### [F-IDE 2016](https://sites.google.com/site/fideworkshop2016/), support data
+### [F-IDE 2016](https://sites.google.com/site/fideworkshop2016/) support data
 
 This repository contains data measuring 8 SMT solvers' performance on the
 [Why3](http://why3.lri.fr/) examples dataset. We record the result returned by [Alt-Ergo](https://alt-ergo.ocamlpro.com/) (versions 0.95.2 and 1.01), [CVC3](http://www.cs.nyu.edu/acsys/cvc3/), [CVC4](http://cvc4.cs.nyu.edu/web/), [veriT](http://www.verit-solver.org/), [Yices](http://yices.csl.sri.com/), and [Z3](https://github.com/Z3Prover/z3) (versions 4.3.2 and 4.4.1). We also measure the time taken
@@ -27,3 +27,9 @@ Make the first figure (stacked barcharts - 60 second timeout). Uses `fig1_data.c
 
 #### `create_stats_df.py`
 Collect data from the JSON files and combine it with the syntax metrics. Save the data as `whygoal_stats.csv`
+
+#### `whygoal_test.csv`, `whygoal_valid_test.csv`
+Disjoint partitions of `whygoal_stats.csv` for testing (25%) and training/validation (75%) respectively
+
+#### `make_fig3.py`
+Use the entire dataset to plot the cumulative time taken for Valid/Invalid/Unknown answers to be returned. Renders `line_graph.pdf` to `paper` folder and prints values for the 99th percentile.
